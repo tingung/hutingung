05fb5dcec8b6822803efc517a8db81ea02227069
tingung <tingung.hu@aurea.com>
2019-05-24 12:39:59 +0800

    Add security violations code and project settings file

---
+++ src/main/java/hutingung/CalendarYear.java
@@ -0,0 +1,17 @@
+package hutingung;
+
+/**
+ * https://www.javacodegeeks.com/2012/09/fixing-common-java-security-code.html
+ *
+ */
+public class CalendarYear {
+    private String[] months;
+
+    public String[] getMonths() {
+        return months;
+    }
+
+    public void setMonths(String[] months) {
+        this.months = months;
+    }
+}
